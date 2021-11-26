e3ad0a2b029d7577645851b15c2f9d729c90ddd6
Joscha Seelig <joscha-seelig@outlook.de>
2021-11-26 18:23:36 +0100

    optimize tab navigation in zsh completion menu

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -1,10 +1,6 @@
-#--------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------
 # ZSH - COMPLETIONS
 # -----------------------------------------------------------------------------------------------------
-
-# Load more completions
-fpath=($DOTFILES/zsh/plugins/zsh-completions/src $fpath)
 
 # Should be called before compinit
 zmodload zsh/complist
@@ -19,22 +15,31 @@ bindkey -M menuselect '^I' vi-insert
 bindkey -M menuselect '^F' accept-and-infer-next-history
 bindkey -M menuselect '^U' undo
 
+bindkey '^[[Z' reverse-menu-complete    # activate shift-tab for cycling back
+
 autoload -U compinit; compinit
-_comp_options+=(globdots) # With hidden files
-
-# Only work with the Zsh function vman
-# See $DOTFILES/zsh/scripts.zsh
-compdef vman="man"
-
+_comp_options+=(globdots)               # With hidden files
 
 # OPTIONS
 # -----------------
+setopt ALWAYS_TO_END                    # place cursor to end after completion
+setopt LIST_PACKED                      # minimize occupied space of completion menu
+setopt MENU_COMPLETE                    # highlight first element of completion menu
+setopt AUTO_LIST                        # list choices on ambiguous completion.
+setopt COMPLETE_IN_WORD                 # complete from both ends of a word.
 
-setopt ALWAYS_TO_END        # place cursor to end after completion
-setopt LIST_PACKED
-setopt MENU_COMPLETE        # highlight first element of completion menu
-setopt AUTO_LIST            # list choices on ambiguous completion.
-setopt COMPLETE_IN_WORD     # complete from both ends of a word.
+# HOST COMPLETION
+# -----------------
+[ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
+[ -r /etc/hosts ] && : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}} || _etc_hosts=()
+hosts=(
+  "$_global_ssh_hosts[@]"
+  "$_ssh_hosts[@]"
+  "$_etc_hosts[@]"
+  "$HOST"
+  localhost
+)
+zstyle ':completion:*:hosts' hosts $hosts
 
 
 # ZSTYLES
@@ -59,19 +64,20 @@ zstyle ':completion:alias-expension:*' completer _expand_alias
 # Use cache for commands which use it
 
 # Allow you to select in a menu
-zstyle ':completion:*' menu select interactive
+zstyle ':completion:*' menu
 
 # Autocomplete options for cd instead of directory stack
 zstyle ':completion:*' complete-options true
 
 zstyle ':completion:*' file-sort modification
 
+# partial completion suggestions
+zstyle ':completion:*' list-suffixes zstyle ':completion:*' expand prefix suffix 
 
 zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
 zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
 zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-# zstyle ':completion:*:default' list-prompt '%S%M matches%s'
 
 # Colors for files and directory
 zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
@@ -90,4 +96,8 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 zstyle ':completion:*' keep-prefix true
 
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
-
+# match partial words
+zstyle ':completion:*' matcher-list \
+    'm:{[:lower:]}={[:upper:]}' \
+    '+r:|[._-]=* r:|=*' \
+    '+l:|=*'

896dc90519eae6c073d6f9c63cd87aa816da0110
Joscha Seelig <joscha-seelig@outlook.de>
2021-11-28 03:13:30 +0100

    add new terminal presets

--- nvim/.config/nvim/lua/setup/toggleterm.lua
+++ nvim/.config/nvim/lua/setup/toggleterm.lua
@@ -7,31 +7,50 @@ local bmap = f.bmap
 local cmd = vim.cmd
 
 require('toggleterm').setup{
-  open_mapping = [[<C-T>]],
+  open_mapping = [[<C-F>]],
   shading_factor = '2',
+  size = 23,
+  shade_terminals = false
 }
 
 function _G.set_terminal_keymaps()
   bmap('t', '<esc>', [[<C-\><C-n>]])
-  bmap('t', 'jk', [[<C-\><C-n>]])
-  bmap('t', '<C-h>', [[<C-\><C-n><C-W>h]])
-  bmap('t', '<C-j>', [[<C-\><C-n><C-W>j]])
-  bmap('t', '<C-k>', [[<C-\><C-n><C-W>k]])
-  bmap('t', '<C-l>', [[<C-\><C-n><C-W>l]])
+  bmap('t', '<C-S-h>', [[<C-\><C-n><C-W>h]])
+  bmap('t', '<C-S-j>', [[<C-\><C-n><C-W>j]])
+  bmap('t', '<C-S-k>', [[<C-\><C-n><C-W>k]])
+  bmap('t', '<C-S-l>', [[<C-\><C-n><C-W>l]])
 end
 
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
--- LAZYGIT SETUP
+-- CUSTOM TERMINALS
+local function create_floating_term(cmd, keymap, id)
+  local terminal = Terminal:new {
+    cmd = cmd,
+    direction = 'float',
+    float_opts = { border = 'curved' },
+  }
+  return terminal
+end
 
-local lazygit = Terminal:new {
-  cmd = 'lazygit',
-  direction = 'float',
-  float_opts = { border = 'curved' },
-}
-
+-- LAZYGIT
+local lazygit = create_floating_term('lazygit', '<Leader>k', 10)
 function _lazygit_toggle()
   lazygit:toggle()
 end
-
 map('n', '<Leader>k', '<cmd>lua _lazygit_toggle()<CR>')
+
+-- HTOP
+local htop = create_floating_term('htop', '<Leader>h', 12)
+function _htop_toggle()
+  htop:toggle()
+end
+map('n', '<Leader>i', '<cmd>lua _htop_toggle()<CR>')
+
+-- CTOP
+local ctop = create_floating_term('ctop', '<Leader>c', 11)
+function _ctop_toggle()
+  ctop:toggle()
+end
+map('n', '<Leader>d', '<cmd>lua _ctop_toggle()<CR>')
 

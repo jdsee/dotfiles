0857a458bc83db4f6942c14baa52996e6b62a7e3
Joscha Seelig <joscha-seelig@outlook.de>
2021-11-28 03:13:43 +0100

    update custom functions

--- nvim/.config/nvim/lua/util/functions.lua
+++ nvim/.config/nvim/lua/util/functions.lua
@@ -1,18 +1,20 @@
 -- NVIM FUNCTIONS
 
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
+local function extend_opt(lh, rh)
   if opts then
     options = vim.tbl_extend("force", options, opts)
   end
+end
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  extend_opt(options, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 local function bmap(mode, lhs, rhs, opts)
   local options = { noremap = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
+  extend_opt(options, opts)
   vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, options)
 end
 

b8fae1c652ff400f7f672a9da39251facdbad848
Joscha Seelig <joscha-seelig@outlook.de>
2021-11-30 01:11:38 +0100

    add highlighting on yank to nvim

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -2,7 +2,16 @@
 
 local cmd = vim.api.nvim_command
 
-cmd([[
-  autocmd FileType help wincmd L    " open help in vertical split
-]])
+-- Open help in vertical split
+cmd [[
+  autocmd FileType help wincmd L
+]]
+
+-- Highlight on yank
+vim.cmd [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]]
 

00c764315101e886bd11d3826a948801de58003c
Joscha Seelig <joscha-seelig@outlook.de>
2021-11-30 01:12:24 +0100

    add move-line keybindings to nvim and update keymap

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -16,11 +16,13 @@ map('i', '<C-S-S>', '<Esc>:wa<CR>a')                              -- save
 map('n', '<C-C>', ':noh<CR>', { nowait = true })                  -- remove visual selection
 
 -- Reload NVIM (TODO: not working)
-vim.cmd[[command! Src lua require('plenary.reload').reload_module('setup', true)]]
+vim.cmd [[
+  command! Src lua require('plenary.reload').reload_module('setup', true)
+]]
 
 -- Navigation
-map('n', 'k', 'gk')                                               -- move up wrapped line
-map('n', 'j', 'gj')                                               -- move down wrapped line
+map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })       -- move up wrapped line
+map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })       -- move down wrapped line
 map('n', '<Leader>e', '$')                                        -- move to end of line
 
 -- Windows / Tabs
@@ -28,13 +30,12 @@ map('n', '<C-S-K>', '<C-W>k')                                     -- go window u
 map('n', '<C-S-J>', '<C-W>j')                                     -- go window down
 map('n', '<C-S-H>', '<C-W>h')                                     -- go window lelft
 map('n', '<C-S-L>', '<C-W>l')                                     -- go window right
-map('n', '<C-S-W>', '<C-W>q')                                     -- close window
 map('n', 'tt', ':tab split<CR>')                                  -- open in new tab
 
 -- Buffers
 map('n', '<C-.>', ':bd<CR>')                                      -- close buffer
 map('n', '<C-,>', ':bd<CR>')                                      -- close buffer
-map('n', '<C-W>', ':bd<CR>')                                      -- close buffer
+map('n', '<C-S-W>', ':bd<CR>')                                      -- close buffer
 
 -- Clipboard
 map('n', '<Leader>y', '"*y')                                      -- copy to system clipboard
@@ -43,4 +44,13 @@ map('n', '<Leader>s', '"*s')                                      -- substitute 
 
 -- Refactoring
 map('x', '<Leader>r', '<Leader>siwa<CR>') -- TODO: not working    -- rename single word/term
+map('n', '<A-j>', ':m .+1<CR>==')                                 -- move line down
+map('n', '<A-k>', ':m .-2<CR>==')                                 -- move line up
+map('i', '<A-j>', '<Esc>:m .+1<CR>==gi')                          -- move line down
+map('i', '<A-k>', ':m .+1<CR>==')                                 -- move line up
+map('v', '<A-j>', ":m '>+1<CR>gv=gv")                             -- move line down
+map('v', '<A-k>', ":m '<-2<CR>gv=gv")                             -- move line up
+
+-- Custom text objects
+map('o', 'ae', ':exec "normal! ggVG"<CR>')                        -- whole buffer text object
 

5bc528e133fe8f0e824cb672559c2b7d3fed6f9f
Joscha Seelig <joscha-seelig@outlook.de>
2021-11-30 01:13:12 +0100

    update zsh exit code prompt

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -4,9 +4,11 @@
 # mkdir && cd
 function mkcd {
   last=$(eval "echo \$$#")
-  if [ ! -n "$last" ]; then
+  if [ ! -n "$last" ]
+  then
     echo "Enter a directory name"
-  elif [ -d $last ]; then
+  elif [ -d $last ]
+  then
     echo "\`$last' already exists"
   else
     mkdir $@ && cd $last
@@ -16,9 +18,14 @@ function mkcd {
 # exit-code prompt
 function check_last_exit_code() {
   local LAST_EXIT_CODE=$?
-  if [[ $LAST_EXIT_CODE -ne 0 ]]; then
+  if [[ $LAST_EXIT_CODE -ne 0 ]]
+  then
     local EXIT_CODE_PROMPT=' '
-    EXIT_CODE_PROMPT+="%{$fg_bold[red]%} $LAST_EXIT_CODE%{$reset_color%}"
+    EXIT_CODE_PROMPT+="%{$fg_bold[red]%}$LAST_EXIT_CODE ●%{$reset_color%}"
+  # unccomment for success indicator
+  # else
+  #   local EXIT_CODE_PROMPT=' '
+  #   EXIT_CODE_PROMPT+="%{$fg_bold[green]%}﬽%{$reset_color%}"
   fi
   echo $EXIT_CODE_PROMPT
 }

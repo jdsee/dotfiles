8372b71cfde32a856ee984ca4f92e550f928380d
Joscha Seelig <joscha-seelig@outlook.de>
2022-04-14 01:40:37 +0200

    configure terminal defaults as autocommand

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -15,3 +15,12 @@ vim.cmd [[
   augroup end
 ]]
 
+-- Terminal Defaults
+vim.cmd [[
+  augroup neovim_terminal
+      autocmd!
+      autocmd TermOpen * startinsert                      "" start terminal in insert mode
+      autocmd TermOpen * :set nonumber norelativenumber   "" disable linenumbers in terminal
+      autocmd TermOpen * nnoremap <buffer> <C-c> i<C-c>   "" use Ctrl-c in terminal
+  augroup END
+]]

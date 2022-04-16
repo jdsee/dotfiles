6fa008ee54ec146181c4ddb2b6094cfbc716ab1f
Joscha Seelig <joscha-seelig@outlook.de>
2022-04-16 15:01:37 +0200

    update refactoring key mappings

--- nvim/.config/nvim/lua/setup/refactoring.lua
+++ nvim/.config/nvim/lua/setup/refactoring.lua
@@ -6,11 +6,9 @@ local map = require('util.functions').map
 require('refactoring').setup()
 require('telescope').load_extension('refactoring')
 
--- Remaps for each of the four debug operations currently offered by the plugin
-map('v', '<Leader>ef', [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR> ]])
-map('v', '<Leader><Leader>ef', [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR> ]])
-map('v', '<Leader>iv', [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR> ]])
-map('v', '<Leader>ri', [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]])
+map('v', '<Leader>re', [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR> ]])
+map('v', '<Leader>ref', [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR> ]])
+map('v', '<Leader>ri', [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR> ]])
 
 map(
 	'n',
@@ -22,10 +20,9 @@ map("v", "<leader>rv", ":lua require('refactoring').debug.print_var({})<CR>")
 map("n", "<leader>rc", ":lua require('refactoring').debug.cleanup({})<CR>")
 
 -- Telescope extension
-
 map(
 	"v",
-	"<leader>rr",
+	"<leader>rm",
 	"<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>"
 )
 

98a782937d12d25edaf7262d965687c3912c27e6
Joscha Seelig <joscha-seelig@outlook.de>
2022-04-22 15:31:55 +0200

    configure fugitive as git mergetool

--- nvim/.config/nvim/lua/setup/fugitive.lua
+++ nvim/.config/nvim/lua/setup/fugitive.lua
@@ -3,6 +3,6 @@
 local map = require('util.functions').map
 
 map('n', '<Leader>gs', '<cmd>G<CR>')
+map('n', 'd>', '<cmd>diffget //2<CR>')
+map('n', 'd<', '<cmd>diffget //3<CR>')
 
-map('n', '<Leader>gh', '<cmd>diffget //2')
-map('n', '<Leader>gh', '<cmd>diffget //3')

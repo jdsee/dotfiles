abc31f9ab54f4f450a25e9ea6c16b050f739ffdd
Joscha Seelig <joscha-seelig@outlook.de>
2022-06-15 22:22:47 +0200

    update neotest mappings

--- nvim/.config/nvim/lua/setup/neotest.lua
+++ nvim/.config/nvim/lua/setup/neotest.lua
@@ -15,20 +15,24 @@ neotest.setup({
   },
 })
 
-M.run_file = function ()
+M.run_all = function ()
   neotest.run.run(vim.fn.expand('%'))
+end
+
+M.run_all_in_file = function ()
+  neotest.run.run(vim.fn.getcwd())
 end
 
 M.debug = function ()
   neotest.run.run({ strategy = 'dap'})
 end
 
-map('n', '<Leader>te', M.run_file)
-map('n', '<Leader>ta', M.run_file)
+map('n', '<Leader>tf', M.run_all_in_file)
+map('n', '<Leader>ta', M.run_all)
 map('n', '<Leader>tj', neotest.run.run)
 map('n', '<Leader>tt', neotest.run.run_last)
 map('n', '<Leader>td', M.debug)
 map('n', '<Leader>tx', neotest.run.stop)
-map('n', '<Leader>to<ESC>', neotest.output.open)
+map('n', '<Leader>to', neotest.output.open)
 map('n', '<Leader>ts', neotest.summary.toggle)
 

9ef2ba4d4bdebc258aa95c230627e05d33ee6ea0
Joscha Seelig <joscha-seelig@outlook.de>
2022-06-24 22:38:06 +0200

    update fugitive keymaps

--- nvim/.config/nvim/lua/setup/fugitive.lua
+++ nvim/.config/nvim/lua/setup/fugitive.lua
@@ -3,6 +3,8 @@
 local map = require('util.functions').map
 
 map('n', '<Leader>gs', '<cmd>G<CR>')
-map('n', 'd>', '<cmd>diffget //2<CR>')
-map('n', 'd<', '<cmd>diffget //3<CR>')
+map('n', 'gd>', '<cmd>diffget //2<CR>')
+map('n', 'gd<', '<cmd>diffget //3<CR>')
+map('n', 'gdl', '<cmd>diffget //2<CR>')
+map('n', 'gdh', '<cmd>diffget //3<CR>')
 

74602c4f50a411ac3878f619695a6148c74b17d9
Joscha Seelig <joscha-seelig@outlook.de>
2022-06-24 22:39:59 +0200

    remove buggy cmp functions

--- nvim/.config/nvim/lua/setup/nvim-cmp.lua
+++ nvim/.config/nvim/lua/setup/nvim-cmp.lua
@@ -1,8 +1,5 @@
 -- NVIM-CMP SETUP
 
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-vim.opt.shortmess:append "c"
-
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 local lspkind = require 'lspkind'
@@ -60,46 +57,6 @@ cmp.setup {
     ghost_text = true,
   }
 }
-
-local function has_words_before()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  return (vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or ''):sub(cursor[2], cursor[2]):match('%s')
-end
-
-local tab_mapping = cmp.mapping(
-  function(fallback)
-    if cmp.visible() then cmp.select_next_item()
-    elseif has_words_before() then cmp.complete()
-    else fallback() end
-  end, {"i", "c"})
-
-local tab_back_mapping = cmp.mapping(
-  function(fallback)
-    if cmp.visible() then cmp.select_prev_item()
-    else fallback() end
-  end, {"i", "c"})
-
--- completion for search
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer'}
-  },
-  mappings = {
-    ['<Tab>'] = tab_mapping,
-    ['<S-Tab>'] = tab_back_mapping,
-  }
-})
-
--- completion for command line
-cmp.setup.cmdline(':', {
-  sources = {
-    { name = 'cmdline'}
-  },
-  mappings = {
-    ['<Tab>'] = tab_mapping,
-    ['<S-Tab>'] = tab_back_mapping,
-  }
-})
 
 -- TODO: set color
 

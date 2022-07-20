0ac09625d084df5b4dc0bdae9ae3ebc4f51c1f02
Joscha Seelig <joscha-seelig@outlook.de>
2022-07-20 12:02:48 +0200

    fix nvim clipboard-copy for linux

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -39,13 +39,13 @@ map('n', '<Leader>O', 'O<Esc>')                                   -- insert line
 map('n', 'gf', ':edit <cfile><cr>')                               -- open non existing files with gf
 
 -- Windows / Tabs
-map('n', '<M-K>', '<C-W>k')                                       -- go window up
-map('n', '<M-J>', '<C-W>j')                                       -- go window down
-map('n', '<M-H>', '<C-W>h')                                       -- go window lelft
-map('n', '<M-L>', '<C-W>l')                                       -- go window right
-map('n', 'tt', ':tab split<CR>')                                  -- open in new tab
+map('n', '<M-K>',     '<C-W>k')                                   -- go window up
+map('n', '<M-J>',     '<C-W>j')                                   -- go window down
+map('n', '<M-H>',     '<C-W>h')                                   -- go window lelft
+map('n', '<M-L>',     '<C-W>l')                                   -- go window right
+map('n', 'tt',        ':tab split<CR>')                           -- open in new tab
 map('n', '<Leader>q', ':bd!<CR>')                                 -- close curent buffer
-map('t', '<C-W>', '<C-\\><C-N><C-W>')                             -- window management in terminal mode
+map('t', '<C-W>',     '<C-\\><C-N><C-W>')                         -- window management in terminal mode
 
 -- Quickfixlist / Locationlist
 map('n', '<Leader>cn', '<CMD>cn<CR>')                             -- go to next quickfix entry
@@ -57,9 +57,10 @@ map('n', '<Leader>cP', '<CMD>cp<CR>')                             -- go to next 
 map('n', '<C-,>', ':bd<CR>')                                      -- close buffer
 
 -- Clipboard
-map('n', '<Leader>y', '"*y')                                      -- copy to system clipboard
-map('n', '<Leader>p', '"*p')                                      -- paste from system clipboard
-map('n', '<Leader>s', '"*s')                                      -- substitute from system clipboard
+local clip_reg = vim.fn.has('mac') == 1 and "*" or "+"
+map('n', '<Leader>y', '"'..clip_reg..'y')                         -- copy to system clipboard
+map('n', '<Leader>p', '"'..clip_reg..'p')                         -- paste from system clipboard
+map('n', '<Leader>s', '"'..clip_reg..'s')                         -- substitute from system clipboard
 
 -- Refactoring
 map('n', '<A-S-j>', ':m .+1<CR>==')                               -- move line down
@@ -70,6 +71,7 @@ map('v', '<A-S-j>', ":m '>+1<CR>gv=gv")                           -- move line d
 map('v', '<A-S-k>', ":m '<-2<CR>gv=gv")                           -- move line up
 
 -- Custom text objects
+-- TODO: these don't work
 map('o', 'af', ':exec "normal! ggVG"<CR>')                        -- whole buffer text object
 map('o', 'if', ':exec "normal! ggVG"<CR>')                        ---
 

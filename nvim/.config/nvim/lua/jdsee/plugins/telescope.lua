689d4426bc1b228d908b8d3efdc54cf1b948d51b
Joscha Seelig <joscha-seelig@outlook.de>
2022-09-24 01:36:20 +0200

    improve zoxide telescope mappings

--- nvim/.config/nvim/lua/jdsee/plugins/telescope.lua
+++ nvim/.config/nvim/lua/jdsee/plugins/telescope.lua
@@ -11,19 +11,36 @@ telescope.setup {
     mappings = {
       i = {
         ["<C-h>"] = "which_key",
-        ["<C-J>"] = "move_selection_next",
-        ["<C-K>"] = "move_selection_previous",
       }
     },
   },
   pickers = {},
-  extensions = {}
+  extensions = {
+    zoxide = {
+      mappings = {
+        ['<CR>'] = {
+          keepinsert = true,
+          action = function(selection)
+            vim.cmd("cd " .. selection.path)
+            vim.cmd("!cd " .. selection.path)
+            builtin.find_files { cwd = selection.path }
+          end,
+        },
+        ['<C-j>'] = {
+          keepinsert = true,
+          action = function(selection)
+            builtin.find_files { cwd = selection.path }
+          end,
+        },
+      }
+    }
+  }
 }
 
 telescope.load_extension 'fzf'
-telescope.load_extension 'file_browser'
 telescope.load_extension 'harpoon'
 telescope.load_extension 'ui-select'
+telescope.load_extension 'zoxide'
 
 M = {}
 
@@ -55,19 +72,30 @@ end
 function M.find_nvim_files()
   builtin.find_files {
     opt = { cwd = "$XDG_HOME/neovim" },
-    hidden = true }
+    hidden = true
+  }
+end
+
+function M.spell_suggestions()
+  builtin.spell_suggest(themes.get_cursor())
 end
 
 -- Keybindings
-vim.keymap.set('n', '<Leader><CR>', M.buffers)                  -- search buffers
-vim.keymap.set('n', '<Leader>ff', M.find_all_files)             -- search files
-vim.keymap.set('n', '<Leader>fn', M.find_nvim_files)            -- search files in neovim config
-vim.keymap.set('n', '<Leader>fg', builtin.live_grep)            -- grep everywhere
-vim.keymap.set('n', '<Leader>fh', builtin.help_tags)            -- search help tags
-vim.keymap.set('n', '<Leader>gb', builtin.git_branches)         -- search git branches
-vim.keymap.set('n', '<Leader>fc', builtin.commands)             -- search command history
-vim.keymap.set('n', '<Leader>/', M.current_buffer_fuzzy_find)   -- grep current buffer
-vim.keymap.set('n', '<Leader>:', builtin.command_history)       -- search command history
-vim.keymap.set('n', '<Leader>fd', builtin.diagnostics)          -- search errors from lsp
-vim.keymap.set('n', '<Leader-a>', M.lsp_code_actions)           -- search code actions in telescope
-vim.keymap.set('n', 'gr', M.lsp_find_references)                -- find references with lsp
+vim.keymap.set('n', '<Leader>fa', builtin.builtin) -- search telescope actions
+vim.keymap.set('n', '<Leader>fj', telescope.extensions.zoxide.list) -- search autojump list
+vim.keymap.set('n', '<Leader>ff', M.find_all_files) -- search files
+vim.keymap.set('n', '<Leader>fn', M.find_nvim_files) -- search files in neovim config
+vim.keymap.set('n', '<Leader>fg', builtin.live_grep) -- grep everywhere
+vim.keymap.set('n', '<Leader>fh', builtin.help_tags) -- search help tags
+vim.keymap.set('n', '<Leader>fp', builtin.git_files) -- search files tracked by git
+vim.keymap.set('n', '<Leader>gb', builtin.git_branches) -- search git branches
+vim.keymap.set('n', '<Leader>gc', builtin.git_commits) -- search git commits
+vim.keymap.set('n', '<Leader>fc', builtin.commands) -- search command history
+vim.keymap.set('n', '<Leader>s', builtin.treesitter) -- search treesitter structure
+vim.keymap.set('n', '<Leader>/', M.current_buffer_fuzzy_find) -- grep current buffer
+vim.keymap.set('n', '<Leader>:', builtin.command_history) -- search command history
+vim.keymap.set('n', '<Leader>fd', builtin.diagnostics) -- search errors from lsp
+vim.keymap.set('n', '<Leader>a', M.lsp_code_actions) -- search code actions in telescope
+vim.keymap.set('n', '<Leader><Tab>', M.buffers) -- search buffers
+vim.keymap.set('n', 'z=', M.spell_suggestions) -- search spell suggestions
+vim.keymap.set('n', 'gr', M.lsp_find_references) -- find references with lsp

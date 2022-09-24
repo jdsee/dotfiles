8288d846dbaed9d028f2ef0e650419d57472d038
Joscha Seelig <joscha-seelig@outlook.de>
2022-09-24 23:53:25 +0200

    set some cool options in nvim

--- nvim/.config/nvim/lua/jdsee/settings.lua
+++ nvim/.config/nvim/lua/jdsee/settings.lua
@@ -1,46 +1,57 @@
+local opt = vim.opt
+
 -- General
-vim.o.number = true -- line numbers
-vim.o.relativenumber = true -- line number relative to position
-vim.o.wrap = false -- don't wrap lines
-vim.o.mouse = 'a' -- mouse support in all modes
-vim.o.hidden = true -- hide buffers without saving
-vim.o.spelllang = 'en_us,de_de'
--- vim.o.shortmess:append 'a'                -- use all message abbreveations
-vim.opt.completeopt = { -- completion options
-  'menu',
-  'menuone',
-  'noselect',
-}
+opt.number = true -- line numbers
+opt.relativenumber = true -- line number relative to position
+opt.wrap = false -- don't wrap lines
+opt.mouse = 'a' -- mouse support in all modes
+opt.hidden = true -- hide buffers without saving
+opt.spelllang = 'en_us,de_de'
+
+-- Windows
+opt.equalalways = false -- no window resize
+
+-- Formatting
+opt.formatoptions = opt.formatoptions
+    - 'a' -- no autoformat
+    - 'o' -- don't continue comments with o
+
+-- Completion
+opt.wildmode = 'longest:full'
+opt.wildoptions = 'pum'
+opt.completeopt = { 'menu', 'menuone', 'noselect' }
+-- TODO: opt.shortmess:append 'a'                -- use all message abbreveations
 
 -- Scrolling
-vim.o.scrolloff = 2 -- keep cursor away from vertical borders
-vim.o.sidescrolloff = 2 -- keep cursor away from horizontal borders
+opt.scrolloff = 2 -- keep cursor away from vertical borders
+opt.sidescrolloff = 2 -- keep cursor away from horizontal borders
 
 -- Tabs and Spaces
 local indent = 2
-vim.o.tabstop = indent
-vim.o.softtabstop = indent
-vim.o.shiftwidth = indent
-vim.o.backspace = indent
-vim.o.smarttab = true
-vim.o.expandtab = true -- use spaces as tab
-vim.o.smartindent = true -- auto indent on insert
-vim.o.autoindent = true -- "
-vim.opt.filetype.indent = true -- "
-vim.o.splitright = true -- open new verticle splits on the right
-vim.o.splitbelow = true -- open new horizontal splits on the bottom
+opt.tabstop = indent
+opt.softtabstop = indent
+opt.shiftwidth = indent
+opt.backspace = { 'indent', 'eol', 'nostop' }
+opt.smarttab = true
+opt.expandtab = true -- use spaces as tab
+opt.smartindent = true -- auto indent on insert
+opt.autoindent = true -- "
+opt.filetype.indent = true -- "
+opt.splitright = true -- open new verticle splits on the right
+opt.splitbelow = true -- open new horizontal splits on the bottom
+opt.joinspaces = false -- prevent some weird extra spaces
 
 -- Style
 vim.cmd [[ colorscheme zenbones ]] -- theme
 vim.cmd [[ highlight Normal guibg=none]] -- make background transparent
-vim.o.termguicolors = true -- true color support
-vim.o.background = 'dark' -- dark bg
-vim.o.syntax = 'on' -- basic syntax highlighting
-vim.o.showmatch = true -- show matching brackets
-vim.o.cursorline = true -- highlight current line
-vim.o.cmdheight = 0 -- hide cmdline when not used
+opt.termguicolors = true -- true color support
+opt.background = 'dark' -- dark bg
+opt.syntax = 'on' -- basic syntax highlighting
+opt.showmatch = true -- show matching brackets
+opt.cursorline = true -- highlight current line
+opt.cmdheight = 0 -- hide cmdline when not used
 
 -- Search and Command
-vim.o.ignorecase = true -- case insensitive matching
-vim.o.inccommand = 'nosplit' -- visualize command while typing
-vim.o.hlsearch = false -- show no highlight on search
+opt.ignorecase = true -- case insensitive matching
+opt.inccommand = 'nosplit' -- visualize command while typing
+opt.hlsearch = false -- show no highlight on search

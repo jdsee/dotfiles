7de88f2d8d50149d94c7af3ccb7aeae41e91eed9
Joscha Seelig <joscha-seelig@outlook.de>
2022-10-14 11:03:37 +0200

    Set cursorline only in active window

--- nvim/.config/nvim/lua/jdsee/autocommands.lua
+++ nvim/.config/nvim/lua/jdsee/autocommands.lua
@@ -51,3 +51,18 @@ vim.api.nvim_create_autocmd(
   pattern = 'lua/plugins.lua',
   command = 'PackerSync',
 })
+
+-- Turn off cursorline on inactive buffers (stolen from TJ)
+local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
+local set_cursorline = function(event, value, pattern)
+  vim.api.nvim_create_autocmd(event, {
+    group = group,
+    pattern = pattern,
+    callback = function()
+      vim.opt_local.cursorline = value
+    end,
+  })
+end
+set_cursorline('WinLeave', false)
+set_cursorline('WinEnter', true)
+set_cursorline('FileType', false, 'TelescopePrompt')

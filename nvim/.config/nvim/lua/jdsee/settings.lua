b833438ca83003b15376a53301e629abb004d938
Joscha Seelig <joscha-seelig@outlook.de>
2022-10-14 11:06:02 +0200

    Set spell and improve other options in nvim

--- nvim/.config/nvim/lua/jdsee/settings.lua
+++ nvim/.config/nvim/lua/jdsee/settings.lua
@@ -6,6 +6,7 @@ opt.relativenumber = true -- line number relative to position
 opt.wrap = false -- don't wrap lines
 opt.mouse = 'a' -- mouse support in all modes
 opt.hidden = true -- hide buffers without saving
+opt.spell = true
 opt.spelllang = 'en_us,de_de'
 
 -- Windows
@@ -15,12 +16,13 @@ opt.equalalways = false -- no window resize
 opt.formatoptions = opt.formatoptions
     - 'a' -- no autoformat
     - 'o' -- don't continue comments with o
+opt.formatoptions:remove 'o'
 
 -- Completion
 opt.wildmode = 'longest:full'
 opt.wildoptions = 'pum'
 opt.completeopt = { 'menu', 'menuone', 'noselect' }
--- TODO: opt.shortmess:append 'a'                -- use all message abbreveations
+-- TODO: opt.shortmess:append 'a'                -- use all message abbreviations
 
 -- Scrolling
 opt.scrolloff = 2 -- keep cursor away from vertical borders
@@ -37,21 +39,20 @@ opt.expandtab = true -- use spaces as tab
 opt.smartindent = true -- auto indent on insert
 opt.autoindent = true -- "
 opt.filetype.indent = true -- "
-opt.splitright = true -- open new verticle splits on the right
+opt.splitright = true -- open new vertical splits on the right
 opt.splitbelow = true -- open new horizontal splits on the bottom
 opt.joinspaces = false -- prevent some weird extra spaces
 
 -- Style
-vim.cmd [[ colorscheme zenbones ]] -- theme
-vim.cmd [[ highlight Normal guibg=none]] -- make background transparent
+vim.cmd [[ colorscheme tokyonight-night ]]
+vim.cmd [[ highlight Normal guibg=none ]] -- make background transparent
 opt.termguicolors = true -- true color support
-opt.background = 'dark' -- dark bg
 opt.syntax = 'on' -- basic syntax highlighting
 opt.showmatch = true -- show matching brackets
 opt.cursorline = true -- highlight current line
-opt.cmdheight = 0 -- hide cmdline when not used
+opt.cmdheight = 0 -- hide command line when not used
 
 -- Search and Command
 opt.ignorecase = true -- case insensitive matching
 opt.inccommand = 'nosplit' -- visualize command while typing
-opt.hlsearch = false -- show no highlight on search
+opt.hlsearch = false -- don't highlight search results
